86d5fcb7a304d6daf4ae2dedcec0de6221130880
aymxdd <aymxdd@gmail.com>
2025-04-24 10:42:14 +0200

    feat: init repo

---
+++ test/Factory.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Factory} from "../src/Factory.sol";
+
+contract FactoryTest is Test {
+    Factory public factory;
+
+    function setUp() public {
+        factory = new Factory();
+        factory.setNumber(0);
+    }
+
+    function test_Increment() public {
+        factory.increment();
+        assertEq(factory.number(), 1);
+    }
+
+    function testFuzz_SetNumber(uint256 x) public {
+        factory.setNumber(x);
+        assertEq(factory.number(), x);
+    }
+}
