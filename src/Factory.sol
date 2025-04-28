3538cecd99c8bc3e7d043ce7b090a0ddb98b7943
aymxdd <aymxdd@gmail.com>
2025-04-28 17:18:01 +0200

    feat(factory): add operation tracking

--- src/Factory.sol
+++ src/Factory.sol
@@ -13,6 +13,8 @@ contract Factory is Ownable {
     uint256 public operationCount = 0;
 
     mapping(uint256 => Operation) public operations;
+    mapping(uint256 => uint256) public fundingProgress;
+    mapping(uint256 => bool) public operationStarted;
 
     struct Operation {
         address opToken;
@@ -22,6 +24,7 @@ contract Factory is Ownable {
     }
 
     event OperationCreated(address indexed opToken, uint256 indexed id, uint256 totalShares);
+    event Invested(address indexed investor, uint256 indexed operationId, uint256 indexed usdcAmount, uint256 sharesBought);
 
     constructor(address _USDC) Ownable(msg.sender) {
         dLEND = new LendDebt();
@@ -35,9 +38,17 @@ contract Factory is Ownable {
         return operations[id];
     }
 
+    function getAmountIn(uint256 operationId, uint256 sharesAmount) public view returns(uint256) {
+        return usdcToEur(operations[operationId].eurPerShares) * sharesAmount;
+    }
+
     function usdcToEur(uint256 usdcAmount) public pure returns(uint256) {
         // TODO: oracle call here to get the actual quote
         return usdcAmount;
+    }
+
+    function isOperationFinished(uint256 id) public view returns(bool) {
+        return operationStarted[id] && fundingProgress[id] >= operations[id].totalShares;
     }
     //**********************************
 
@@ -69,15 +80,26 @@ contract Factory is Ownable {
         return address(newOp);
     }
 
+    function startOperation(uint256 id) external onlyOwner {
+        operationStarted[id] = true;
+    }
+
     function invest(uint256 id, uint256 sharesAmount) external {
+        require(operationStarted[id] == true);
         require(operations[id].totalShares > 0);
+        require(fundingProgress[id] + sharesAmount <= operations[id].totalShares);
         require(sharesAmount > 0);
 
-        uint256 cost = usdcToEur(operations[id].eurPerShares) * sharesAmount;
+        uint256 cost = getAmountIn(id, sharesAmount);
         require(USDC.allowance(msg.sender, address(this)) >= cost);
 
         USDC.transferFrom(msg.sender, address(this), cost);
+
+        fundingProgress[id] += sharesAmount;
+
         dLEND.mint(msg.sender, id, sharesAmount, "");
+
+        emit Invested(msg.sender, id, cost, sharesAmount);
     }
     //**********************************
 }
