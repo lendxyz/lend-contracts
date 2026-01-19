d567fd9681037bb7e35220f2acbc2ad07122e824
aymxdd <aymxdd@gmail.com>
2026-01-19 14:11:08 +0100

    chore(deps): move to upgradeable OZ contracts

--- script/mainnet/DeployFacetUpgrade.s.sol
+++ script/mainnet/DeployFacetUpgrade.s.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.27;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
+import {FactoryDiamondCuts} from "../common/FactoryDiamondCuts.s.sol";
+import {IDiamondCut} from "../../src/interfaces/IDiamondCut.sol";
 
 import {Admin} from "../../src/facets/Admin.sol";
 import {Getters} from "../../src/facets/Getters.sol";
@@ -9,7 +11,7 @@ import {Invest} from "../../src/facets/Invest.sol";
 import {Operations} from "../../src/facets/Operations.sol";
 import {Ownership} from "../../src/facets/Ownership.sol";
 
-contract DeployFacetUpgrade is Script {
+contract DeployFacetUpgrade is Script, FactoryDiamondCuts {
     function setUp() public {}
 
     function run() public {
@@ -18,7 +20,17 @@ contract DeployFacetUpgrade is Script {
         // Deploy new facets
         Invest investFacet = new Invest();
 
-        console2.log("New invest facet deploy:", address(investFacet));
+        console.log("New invest facet deploy:", address(investFacet));
+
+        IDiamondCut.FacetCut[] memory cut = new IDiamondCut.FacetCut[](1);
+
+        cut[0] = getInvestFacets(address(investFacet), IDiamondCut.FacetCutAction.Replace);
+
+        bytes memory data = abi.encodeWithSelector(IDiamondCut.diamondCut.selector, cut, address(0), "");
+
+        console.log("Encoded call data:");
+        console.logBytes(data);
+
 
         vm.stopBroadcast();
     }
