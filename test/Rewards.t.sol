cb7b605414f34361acf56f2db24fdd7046219a4b
aymxdd <aymxdd@gmail.com>
2026-03-02 14:18:02 +0100

    feat(tests): more robust rewards testing

--- test/Rewards.t.sol
+++ test/Rewards.t.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.27;
 import {Test, console} from "forge-std/Test.sol";
 import {TestBase} from "./TestBase.t.sol";
 import {MerkleHelper} from "./MerkleHelper.sol";
+import {LendRewards} from "../src/Rewards.sol";
 
 contract RewardsTest is Test, TestBase, MerkleHelper {
     function setUp() public override(TestBase) {
@@ -16,25 +17,62 @@ contract RewardsTest is Test, TestBase, MerkleHelper {
         bytes32 root = getRoot(rewardUsers, rewardAmounts);
         uint256 totalAllocation = 600e6;
         uint256 epoch = 1;
-        uint256 opId = 1;
+        uint256 opId = 2;
 
         // Distribute
         vm.startPrank(admin);
         usdc.approve(address(rewards), totalAllocation);
+        vm.expectEmit(address(rewards));
+        emit LendRewards.RewardsDistributed(opId, epoch, totalAllocation);
         rewards.distributeOpRewards(opId, epoch, root, totalAllocation);
         vm.stopPrank();
 
         assertEq(usdc.balanceOf(address(rewards)), totalAllocation);
 
-        // Claim [cite: 39]
+        // Claim
         bytes32[] memory proof = getProof(rewardUsers, rewardAmounts, 2);
 
         address ru = rewardUsers[2];
         uint256 ra = rewardAmounts[2];
 
-        vm.prank(ru);
+        vm.startPrank(ru);
+        vm.expectEmit(address(rewards));
+        emit LendRewards.Claimed(opId, ru, ra);
         rewards.claimOpEpoch(opId, ru, epoch, ra, proof);
+        vm.stopPrank();
 
+        assertEq(rewards.opClaimed(opId, epoch,ru), true);
+        assertEq(usdc.balanceOf(ru), ra);
+    }
+
+    function test_DistributeAndClaimRefRewards() public {
+        bytes32 root = getRoot(rewardUsers, rewardAmounts);
+        uint256 totalAllocation = 600e6;
+        uint256 epoch = 1;
+
+        // Distribute
+        vm.startPrank(admin);
+        usdc.approve(address(rewards), totalAllocation);
+        vm.expectEmit(address(rewards));
+        emit LendRewards.RefRewardsDistributed(epoch, totalAllocation);
+        rewards.distributeRefRewards(epoch, root, totalAllocation);
+        vm.stopPrank();
+
+        assertEq(usdc.balanceOf(address(rewards)), totalAllocation);
+
+        // Claim
+        bytes32[] memory proof = getProof(rewardUsers, rewardAmounts, 2);
+
+        address ru = rewardUsers[2];
+        uint256 ra = rewardAmounts[2];
+
+        vm.startPrank(ru);
+        vm.expectEmit(address(rewards));
+        emit LendRewards.ClaimedRef(ru, ra);
+        rewards.claimRefEpoch(ru, epoch, ra, proof);
+        vm.stopPrank();
+
+        assertEq(rewards.refClaimed(epoch,ru), true);
         assertEq(usdc.balanceOf(ru), ra);
     }
 }
