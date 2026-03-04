5d722dc8813353bba5d9ff48745aa284303b28cf
aymxdd <aymxdd@gmail.com>
2026-03-04 16:00:36 +0100

    feat(rewards): add tests for simple batch claim

--- test/Rewards.t.sol
+++ test/Rewards.t.sol
@@ -30,6 +30,36 @@ contract RewardsTest is Test, TestBase, MerkleHelper {
         vm.stopPrank();
     }
 
+    function createMultipleOpDistribution() public {
+        bytes32 rootEpoch1 = getRoot(rewardUsers, rewardAmounts);
+        bytes32 rootEpoch2 = getRoot(rewardUsers, rewardAmounts);
+        bytes32 rootEpoch3 = getRoot(rewardUsers, rewardAmounts);
+
+        deal(address(usdc), address(admin), totalAllocation * 3);
+
+        vm.startPrank(admin);
+        usdc.approve(address(rewards), totalAllocation * 3);
+        rewards.distributeOpRewards(opId, 1, rootEpoch1, totalAllocation);
+        rewards.distributeOpRewards(opId, 2, rootEpoch2, totalAllocation);
+        rewards.distributeOpRewards(opId, 3, rootEpoch3, totalAllocation);
+        vm.stopPrank();
+    }
+
+    function createMultipleRefDistribution() public {
+        bytes32 rootEpoch1 = getRoot(rewardUsers, rewardAmounts);
+        bytes32 rootEpoch2 = getRoot(rewardUsers, rewardAmounts);
+        bytes32 rootEpoch3 = getRoot(rewardUsers, rewardAmounts);
+
+        deal(address(usdc), address(admin), totalAllocation * 3);
+
+        vm.startPrank(admin);
+        usdc.approve(address(rewards), totalAllocation * 3);
+        rewards.distributeRefRewards(1, rootEpoch1, totalAllocation);
+        rewards.distributeRefRewards(2, rootEpoch2, totalAllocation);
+        rewards.distributeRefRewards(3, rootEpoch3, totalAllocation);
+        vm.stopPrank();
+    }
+
     function createRefDistribution() public {
         bytes32 root = getRoot(rewardUsers, rewardAmounts);
 
@@ -83,6 +113,30 @@ contract RewardsTest is Test, TestBase, MerkleHelper {
         assertEq(usdc.balanceOf(ru), ra);
     }
 
+    function test_DistributeAndClaimMultipleOpRewards() public {
+        createMultipleOpDistribution();
+
+        bytes32[] memory proofEpoch1 = getProof(rewardUsers, rewardAmounts, rewIndex);
+        bytes32[] memory proofEpoch2 = getProof(rewardUsers, rewardAmounts, rewIndex);
+        bytes32[] memory proofEpoch3 = getProof(rewardUsers, rewardAmounts, rewIndex);
+
+        LendRewards.ClaimData[] memory proofs = new LendRewards.ClaimData[](3);
+        proofs[0] = LendRewards.ClaimData(1, ra, proofEpoch1);
+        proofs[1] = LendRewards.ClaimData(2, ra, proofEpoch2);
+        proofs[2] = LendRewards.ClaimData(3, ra, proofEpoch3);
+
+        vm.startPrank(ru);
+        vm.expectEmit(address(rewards));
+        emit LendRewards.Claimed(opId, ru, ra * 3);
+        rewards.claimOpEpochs(opId, ru, proofs);
+        vm.stopPrank();
+
+        assertEq(rewards.opClaimed(opId, 1, ru), true);
+        assertEq(rewards.opClaimed(opId, 2, ru), true);
+        assertEq(rewards.opClaimed(opId, 3, ru), true);
+        assertEq(usdc.balanceOf(ru), ra * 3);
+    }
+
     function test_DistributeAndClaimRefRewards() public {
         bytes32 root = getRoot(rewardUsers, rewardAmounts);
 
@@ -107,6 +161,30 @@ contract RewardsTest is Test, TestBase, MerkleHelper {
 
         assertEq(rewards.refClaimed(epoch, ru), true);
         assertEq(usdc.balanceOf(ru), ra);
+    }
+
+    function test_DistributeAndClaimMultipleRefRewards() public {
+        createMultipleRefDistribution();
+
+        bytes32[] memory proofEpoch1 = getProof(rewardUsers, rewardAmounts, rewIndex);
+        bytes32[] memory proofEpoch2 = getProof(rewardUsers, rewardAmounts, rewIndex);
+        bytes32[] memory proofEpoch3 = getProof(rewardUsers, rewardAmounts, rewIndex);
+
+        LendRewards.ClaimData[] memory proofs = new LendRewards.ClaimData[](3);
+        proofs[0] = LendRewards.ClaimData(1, ra, proofEpoch1);
+        proofs[1] = LendRewards.ClaimData(2, ra, proofEpoch2);
+        proofs[2] = LendRewards.ClaimData(3, ra, proofEpoch3);
+
+        vm.startPrank(ru);
+        vm.expectEmit(address(rewards));
+        emit LendRewards.ClaimedRef(ru, ra * 3);
+        rewards.claimRefEpochs(ru, proofs);
+        vm.stopPrank();
+
+        assertEq(rewards.refClaimed(1, ru), true);
+        assertEq(rewards.refClaimed(2, ru), true);
+        assertEq(rewards.refClaimed(3, ru), true);
+        assertEq(usdc.balanceOf(ru), ra * 3);
     }
 
     function test_CannotClaimMoreOpRewards() public {
