abaea7127d34802ea028747205801254e0aa2122
aymxdd <aymxdd@gmail.com>
2026-03-24 16:25:45 +0100

    feat(tests): add fiatInvest tests

--- test/TestBase.t.sol
+++ test/TestBase.t.sol
@@ -62,6 +62,23 @@ contract TestBase is Test, FactoryDiamondCuts, Constants {
         vm.stopPrank();
     }
 
+    function getMintSignatureFiat(address _user, uint256 _opId, uint256 _amount, string memory _nonce)
+        public
+        returns (bytes memory)
+    {
+        vm.startPrank(backendSigner);
+
+        bytes32 digest =
+            keccak256(abi.encodePacked("FIAT_INVEST", address(factory), block.chainid, _opId, _user, _amount, _nonce));
+        bytes32 hash = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", digest));
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(backendSignerPk, hash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+
+        vm.stopPrank();
+
+        return signature;
+    }
+
     function getMintSignature(address _user, uint256 _opId, uint256 _amount, string memory _nonce)
         public
         returns (bytes memory)
